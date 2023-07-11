902155f1aeb5d71ec04c1b9685ec213691c56a0a
jfuxman <137911083+jfuxman@users.noreply.github.com>
2023-07-10 20:44:03 -0400

    Delete fromTxttoFasta.py

--- fromTxttoFasta.py
+++
@@ -1,7 +0,0 @@
-with open('Gene_Transcript_Seq.txt') as f1, open ('Transcript_Seq.fasta', 'w') as o1:
-	f1.readline()
-	for line in f1:
-		line = line.rstrip().split('\t')
-		if line[2] == 'Sequence unavailable':
-			continue
-		o1.write('>' + line[0] + '_' + line[1] + '\n' + line[2] + '\n')
